94e613c143ff90a588f88f917f4692c1c2d30f29
root <root@SURFACE.>
2024-03-20 14:14:30 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(object,key,value){
+    object[key]=value;
+    return object;
+}
+function destructivelyUpdateEMployeeWithKeyAndValue(object,key,value){
+    var newObj= Object.assign({},object,{[key]:value});
+    return newObj;
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+  }
+  function deleteFromObjectByKey(object, key) {
+    var newObj = Object.assign({}, object);
+    delete newObj[key];
+    return newObj;
+  }
+  function destructivelyDeleteFromObjectByKey(object, key) {
+    delete object[key];
+    return object;
+  }
